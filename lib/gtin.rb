6ddfc72e121519b018710b07706ee9c7931e6883
Hans Masing <hansgit@masing.net>
2010-04-26 09:29:00 -0400

    Some cleanup of pre-coffee stupidity.

--- lib/gtin.rb
+++ lib/gtin.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-module Gtin
-  VERSION = '0.0.1'
+module GTIN
+  VERSION = '0.1.1'
 end
